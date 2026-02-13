b74e9116351bcaac5a242fd3befeaa67bf43d666
daniel <dwho231@uky.edu>
2026-02-13 13:46:47 -0500

    implement cubeMap getColor function

--- ray/scene/cubeMap.cpp
+++ ray/scene/cubeMap.cpp
@@ -7,7 +7,56 @@ extern TraceUI *traceUI;
 glm::dvec3 CubeMap::getColor(ray r) const {
   // YOUR CODE HERE
   // FIXME: Implement Cube Map here
-  return glm::dvec3();
+
+  glm::dvec3 dir = r.getDirection();
+  double absX = fabs(dir.x);
+  double absY = fabs(dir.y);
+  double absZ = fabs(dir.z);
+  int face;
+  double u, v;
+
+  // Find the major axis
+  if (absX >= absY && absX >= absZ) {
+    // X
+    if (dir.x > 0) {
+      face = 0; // +X
+      u = -dir.z / absX;
+    } else {
+      face = 1; // -X
+      u = dir.z / absX;
+    }
+	v = -dir.y / absX;
+  } else if (absY >= absX && absY >= absZ) {
+    // Y
+    if (dir.y > 0) {
+      face = 2; // +Y
+      v = dir.z / absY;
+    } else {
+      face = 3; // -Y
+      v = -dir.z / absY;
+    }
+	u = dir.x / absY;
+  } else {
+    // Z
+    if (dir.z > 0) {
+      face = 4; // +Z
+      u = dir.x / absZ;
+    } else {
+      face = 5; // -Z
+      u = -dir.x / absZ;
+    }
+	v = -dir.y / absZ;
+  }
+
+  // Convert u,v from [-1,1] to [0,1]
+  u = 0.5 * (u + 1.0);
+  v = 0.5 * (v + 1.0);
+
+  // In case there is no map for this face
+  if (!tMap[face])
+    return glm::dvec3(1.0, 1.0, 1.0); 
+
+  return tMap[face]->getMappedValue(glm::dvec2(u, v));
 }
 
 CubeMap::CubeMap() {}
